c7f17c1091911d9db09aa72dab6cc0c407560837
Eugen Ciur <eugen@papermerge.com>
2021-09-25 06:44:37 +0200

    list nodes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-
   async model() {
-    let response = await fetch('/api/folder.json');;
+    let response = await fetch('/api/folder.json');
     let { data } = await response.json();
 
     return data.map((model) => {
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      extension: 'scss',
+    },
   });
-
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
