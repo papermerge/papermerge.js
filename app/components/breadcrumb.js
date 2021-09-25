2bd48cead8ceabb8f6b6344b8197028b468d0e48
Eugen Ciur <eugen@papermerge.com>
2021-09-25 11:36:59 +0200

    Breadcrumb component with state

--- app/components/breadcrumb.js
+++ app/components/breadcrumb.js
@@ -4,7 +4,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class BreadcrumbComponent extends Component {
-  @tracked path = A([]);
+  @tracked path = A([{'id': 1, 'title': 'Some Folder 1'}, {'id': 2, 'title': 'Some Folder 2'}]);
 
   @action
   change(node_id) {
