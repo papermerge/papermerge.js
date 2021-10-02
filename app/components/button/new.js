cbf21a1ebfd4c29707befa1e979a11093429963e
Eugen Ciur <eugen@papermerge.com>
2021-10-02 09:23:55 +0200

    minor formatting

--- app/components/button/new.js
+++ app/components/button/new.js
@@ -3,25 +3,25 @@ import Component from '@glimmer/component';
 
 class ButtonNewComponent extends Component {
   /*
-    "New Button" component. Renders a button as either
-    html <a> tag or as <button>.
+  "New Button" component. Renders a button as either
+  html <a> tag or as <button>.
 
-    Arguments:
-      @route - if `route` argument is provided, button will
-        be rendered as <a>.
-      @onClick - if `onClick` argument is provided - button
-        will be rendered as <button>.
-      @text - button's text. Default value is "New".
+  Arguments:
+    @route - if `route` argument is provided, button will
+      be rendered as <a>.
+    @onClick - if `onClick` argument is provided - button
+      will be rendered as <button>.
+    @text - button's text. Default value is "New".
 
-    Examples:
+  Examples:
 
-      Render component as <button> with `onClick` handler:
+    Render component as <button> with `onClick` handler:
 
-        <Button::New @onClick={{this.onToggleNew}} />
+      <Button::New @onClick={{this.onToggleNew}} />
 
-      Render componet as <a> with given route:
+    Render componet as <a> with given route:
 
-        <Button::New @route="automates.add" />
+      <Button::New @route="automates.add" />
   */
 
   get text() {
