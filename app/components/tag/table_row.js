4f8341e5ba526225a41dba0c94f788d9a9c93ae2
Eugen Ciur <eugen@papermerge.com>
2021-10-02 07:22:40 +0200

    pretty good tag table

--- app/components/tag/table_row.js
+++ app/components/tag/table_row.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 
 export default class TableRowComponent extends Component {
+  // keeps track of the ID of the tag currently
+  // being edited i.e. in edit mode
   @tracked edit_mode_id = 0;
+  @service store;
 
   @action
   async onRemove(tag) {
@@ -13,11 +17,39 @@ export default class TableRowComponent extends Component {
 
   @action
   onEdit(tag) {
+    console.log(`TAG ID = ${tag.id}`);
     this.edit_mode_id = tag.id;
   }
 
   @action
   onCancel() {
     this.edit_mode_id = undefined;
+  }
+
+  @action
+  async onSaveChanges(tag) {
+    let that = this;
+
+    if (!tag) {
+      console.warn(
+        "onSaveChanges received an undefined tag object"
+      );
+      return;
+    }
+
+    this.edit_mode_id = undefined;
+
+    if (tag.id) {
+      this.store.findRecord('tag', tag.id).then(found_tag => {
+        found_tag.name = tag.name;
+        found_tag.description = tag.description;
+        found_tag.save();
+      });
+    } else {
+      console.warn(
+        `onSaveChanges received tag=${tag} object without tag ID`
+      );
+      return;
+    }
   }
 }
