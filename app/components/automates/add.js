5372f8a36510999c0b265eca851824f3ae1463c7
Eugen Ciur <eugen@papermerge.com>
2021-10-03 07:54:18 +0200

    automate add form

--- app/components/automates/add.js
+++ app/components/automates/add.js
@@ -10,11 +10,29 @@ class AddAutomateComponent extends Component {
   */
 
   @service store;
+  @service router;
 
   @action
   onSubmit() {
-  }
+    /*
+    Creates new automate and redirects to automates
+    index view.
+    */
+    let automate = {
+      name: this.name,
+      match: this.match,
+      dst_folder: this.dst_folder,
+      is_case_sensitive: this.is_case_sensitive,
+      matching_algorithm: this.matching_algorithm
+    };
 
+    this.store.createRecord(
+      'automate',
+      automate
+    ).save();
+
+    this.router.transitionTo('automates');
+  }
 }
 
 export default AddAutomateComponent;
