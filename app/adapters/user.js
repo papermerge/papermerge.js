7bf94ad16263909c4f4f455c32169c78afbb30b3
Eugen Ciur <eugen@papermerge.com>
2021-10-23 16:46:46 +0200

    Can now create users and change their password

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -3,14 +3,15 @@ import ApplicationAdapter from './application';
 
 export default class UserAdapter extends ApplicationAdapter {
 
-  _defaultContentType = 'application/json';
-
   changePassword(model, new_password) {
     const url = this.buildURL('user', model.id) + 'change-password/';
 
     return this.ajax(url, 'POST',  {
       data: {
         'password': new_password
+      },
+      headers: {
+        'Content-Type': 'application/json'
       }
     });
   }
