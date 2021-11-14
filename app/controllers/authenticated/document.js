e52793044fd789c751327c20cd5ecf4816891114
Eugen Ciur <eugen@papermerge.com>
2021-11-14 10:55:36 +0100

    now can display on left side - commander and viewer

---
+++ app/controllers/authenticated/document.js
@@ -0,0 +1,23 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from "@glimmer/tracking";
+import { inject as service } from '@ember/service';
+
+
+export default class DocumentController extends Controller {
+
+  @service currentUser;
+  @tracked extranode_id = null;
+  queryParams = ['extranode_id']
+
+  @action
+  onPanelToggle() {
+
+    if (this.extranode_id) {
+      this.extranode_id = null;
+    } else {
+      // TODO: get home folder ID from this.currentUser;
+      this.extranode_id = 75;
+    }
+  }
+}
--- app/routes/authenticated/document.js
+++ app/routes/authenticated/document.js
@@ -1,23 +1,44 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import RSVP from 'rsvp';
 
 
 export default class DocumentRoute extends Route {
   @service store;
 
+  queryParams = {
+    extranode_id: {
+      refreshModel: true
+    }
+  };
+
   async model(params) {
     let doc_adapter,
       page_adapter,
+      node_adapter,
+      extranode,
       document_version,
       pages,
       pages_with_url;
 
+    node_adapter = this.store.adapterFor('node');
     page_adapter = this.store.adapterFor('page');
     doc_adapter = this.store.adapterFor('document');
 
     document_version  = await doc_adapter.getDocumentVersion(params.document_id);
     pages = await document_version.pages;
     pages_with_url = await page_adapter.loadBinaryImages(pages);
+
+    if (params.extranode_id) {
+
+      extranode = await node_adapter.findNode(params.extranode_id)
+
+      return {
+        'document_version': document_version,
+        'pages': pages_with_url,
+        'extranode': extranode
+      };
+    }
 
     return {
       'document_version': document_version,
@@ -27,5 +48,22 @@ export default class DocumentRoute extends Route {
 
   renderTemplate() {
     this.render('authenticated.viewer');
+  }
+
+  setupController(controller, model) {
+    let _controller = this.controllerFor('authenticated.document'),
+      _auth_controller = this.controllerFor('authenticated');
+
+    if (model.extranode) {
+      console.log("Document controller setting dualpanel_mode to true");
+      _controller.set('dualpanel_mode', true);
+      _controller.set('extranode', model.extranode);
+    } else {
+      _controller.set('dualpanel_mode', false);
+      _controller.set('extranode', undefined);
+    }
+
+    _controller.set('document_version', model.document_version);
+    _controller.set('pages', model.pages);
   }
 }
