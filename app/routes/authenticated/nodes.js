930bf57424ea4b2a4de230f30a251d4adc1e6dfc
Eugen Ciur <eugen@papermerge.com>
2021-12-04 07:59:56 +0100

    activate document viewer on the right side

--- app/routes/authenticated/nodes.js
+++ app/routes/authenticated/nodes.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import RSVP from 'rsvp';
 
 import { getPanelInfo } from './utils';
 
@@ -22,18 +21,13 @@ export default class FolderRoute extends Route {
   };
 
   async model(params) {
-    let adapter,
-      doc_adapter,
-      page_adapter,
-      pages,
-      pages_with_url,
-      document_version,
+    let page_adapter,
+      extra_doc,
+      extra_last_version,
+      extra_pages_with_url,
       context = {};
 
-    adapter = this.store.adapterFor('node');
     page_adapter = this.store.adapterFor('page');
-    doc_adapter = this.store.adapterFor('document');
-
     await this.currentUser.loadCurrentUser();
 
     context = await getPanelInfo({
@@ -42,23 +36,28 @@ export default class FolderRoute extends Route {
       page: params.page
     });
 
-
     if (params.extra_id && params.extra_type === 'doc') {
-      document_version  = await doc_adapter.getDocumentVersion(params.extra_id);
-      pages = await document_version.pages;
-      pages_with_url = await page_adapter.loadBinaryImages(pages);
-      this.dualpanel_mode = true;
-
-      return RSVP.hash({
-        node: adapter.findNode(params.node_id),
-        home_folder: this.currentUser.user.home_folder,
-        pages: pages_with_url,
-        document_version: document_version
-      });
+      extra_doc  = await this.store.findRecord(
+        'document',
+        params.extra_id,
+        { reload: true }
+      );
+      extra_last_version = extra_doc.last_version
+      extra_pages_with_url = await page_adapter.loadImages(
+        extra_last_version.pages
+      );
+      context['extra'] = {
+        'doc': extra_doc,
+        'document_versions': extra_doc.versions,
+        'last_document_version': extra_last_version,
+        'pages': extra_pages_with_url,
+      }
+      context['home_folder'] = this.currentUser.user.home_folder;
+      return context;
     }
 
     if (params.extra_id && params.extra_type === 'folder') {
-      context['extranode'] = await getPanelInfo({
+      context['extra'] = await getPanelInfo({
         store: this.store,
         node_id: params.extra_id,
         page: 1
@@ -73,22 +72,9 @@ export default class FolderRoute extends Route {
 
   setupController(controller, model) {
 
-    let _controller = this.controllerFor('authenticated.nodes'),
-      _auth_controller = this.controllerFor('authenticated');
+    let _auth_controller = this.controllerFor('authenticated');
 
-    if (model.extranode) {
-      _controller.set('extranode', model.extranode);
-    } else if (model.document_version) {
-      _controller.set('document_version', model.document_version);
-      _controller.set('pages', model.pages);
-      _controller.set('extranode', model.document_version.document);
-    } else {
-      _controller.set('extranode', undefined);
-    }
-    _controller.set('mainnode', model.current_node);
     _auth_controller.set('home_folder', model.home_folder);
-    _controller.set('children', model.children);
-    _controller.set('pagination', model.pagination);
   }
 
 }
