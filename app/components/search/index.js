289318e5452970ca869f0c683185dab17a24ab00
Eugen Ciur <eugen@papermerge.com>
2022-01-08 08:37:47 +0100

    remove comments and console.log messages

--- app/components/search/index.js
+++ app/components/search/index.js
@@ -4,36 +4,12 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { A } from '@ember/array';
 
-// https://discuss.emberjs.com/t/how-to-force-re-render-a-glimmer-component/18150
+
 export default class SearchComponent extends Component {
   @service requests;
   @service router;
   @tracked query;
   @tracked autocomplete_items = A([]);
-  /*
-  @tracked autocomplete_items = A([
-    {
-      'title': 'invoice.pdf',
-      'type': 'document',
-      'path': 'Home / My Documents / Invoices'
-    },
-    {
-      'title': 'invoice2.pdf',
-      'type': 'document',
-      'path': 'Home / My Documents'
-    },
-    {
-      'title': 'invoice3.pdf',
-      'type': 'document',
-      'path': 'Home / My Documents / Invoices'
-    },
-    {
-      'title': 'invoice.pdf',
-      'type': 'document',
-      'path': 'Home'
-    },
-  ]);
-  */
 
   @action
   async doSearch() {
@@ -57,7 +33,6 @@ export default class SearchComponent extends Component {
 
   @action
   openItem(item) {
-    console.log(`click title=${item.title} ID=${item.document_id}`);
     if (item.type == 'document') {
       this.router.transitionTo('authenticated.document', item.document_id);
       this._reset();
