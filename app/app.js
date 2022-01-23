253c5a5603b407af5ebadf9be8ad844440491a7c
Eugen Ciur <eugen@papermerge.com>
2022-01-23 07:03:36 +0100

    Add application loader indicator.

--- app/app.js
+++ app/app.js
@@ -4,10 +4,29 @@ import loadInitializers from 'ember-load-initializers';
 import config from 'papermerge/config/environment';
 import 'bootstrap';
 
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  ready() {
+    /*
+      Hide rotating spinner when application finished loading and it is ready
+
+      Initial index.html has as background a rotating spinner which
+      indicates that application is loading:
+
+      <div class="loading">
+        <div class="loader"></div>
+      </div>
+    */
+    let divs = document.getElementsByClassName('loading');
+
+    if (divs.length > 0) {
+      divs[0].style.display = 'None';
+    }
+  }
 }
 
 loadInitializers(App, config.modulePrefix);
