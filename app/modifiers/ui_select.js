059da920ed8bb068e949b8d038555129f9e4efb1
Eugen Ciur <eugen@papermerge.com>
2022-02-01 16:04:32 +0200

    pretty good select/unselect nodes feature

--- app/modifiers/ui_select.js
+++ app/modifiers/ui_select.js
@@ -2,6 +2,60 @@ import { action } from '@ember/object';
 import Modifier from 'ember-modifier';
 
 
+class Rectangle {
+
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = parseInt(width);
+    this.height = parseInt(height);
+  }
+
+  intersect(rect) {
+    /*
+    Returns true if this rectangle intersects with rect.
+    Two rectangle intersect if one of them has a point inside other.
+    */
+    if (this.contains_point(rect.x, rect.y)) {
+      return true;
+    }
+
+    if (this.contains_point(rect.x + rect.width, rect.y)) {
+      return true;
+    }
+
+    if (this.contains_point(rect.x + rect.width, rect.y + rect.height)) {
+      return true;
+    }
+
+    if (this.contains_point(rect.x, rect.y + rect.height)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  contains_point(x, y) {
+    /*
+      Is point (x, y) inside this rectangle ?
+    */
+    let x_is_within = false, y_is_within = false;
+
+
+
+    if (this.x <= x && x <= this.x + this.width) {
+      x_is_within = true;
+    }
+
+    if (this.y <= y && y <= this.y + this.height) {
+      y_is_within = true;
+    }
+
+    return x_is_within && y_is_within;
+  }
+}
+
+
 class UISelect {
   /**
     Desktop like select
@@ -21,6 +75,9 @@ class UISelect {
     this.current_y = 0;
     this.parent = parent_selector;
     this.select_div = document.getElementById('ui-select');
+    this.nodes_arr = Array.from(
+      document.getElementsByClassName('node')
+    );
   }
 
   init(x, y) {
@@ -39,7 +96,7 @@ class UISelect {
   }
 
   update(x, y) {
-    let height, width;
+    let height, width, top, left, at_least_one_selected = false;
 
 
     this.show(x, y);
@@ -53,17 +110,84 @@ class UISelect {
 
       if (this.current_y <  this.start_y) {
         this.top = `${this.current_y + 7}px`;
+        top = this.current_y + 7;
       } else {
         this.top = `${this.start_y}px`;
+        top = this.start_y;
       }
       if (this.current_x <  this.start_x) {
         this.left = `${this.current_x + 7}px`;
+        left = this.current_x + 7;
       } else {
         this.left = `${this.start_x}px`;
+        left = this.start_x;
       }
       this.width = `${width}px`;
       this.height = `${height}px`;
+
+      const { selected_nodes, unselected_nodes } = this.get_nodes_selection(
+        new Rectangle(left, top, width, height)
+      );
+
+      this.select_nodes(selected_nodes);
+      this.unselect_nodes(unselected_nodes);
     }
+  }
+
+  get_nodes_selection(selection_rect) {
+    /**
+      selection_rect is instance of utils.MgRect
+    **/
+    let selected_nodes = [], unselected_nodes = [];
+
+    this.nodes_arr.forEach(element => {
+      let _r, rect;
+
+      _r = element.getBoundingClientRect();
+      rect = new Rectangle(_r.x, _r.y, _r.width, _r.height);
+
+      if (selection_rect.intersect(rect)) {
+        selected_nodes.push(element);
+      } else {
+        unselected_nodes.push(element);
+      }
+    });
+
+    return {selected_nodes, unselected_nodes};
+  }
+
+  select_nodes(elements) {
+    let input_el;
+
+    elements.forEach(element => {
+      input_el = element.querySelector('input');
+      if (input_el && !input_el.checked) {
+        input_el.click();
+      }
+    });
+  }
+
+  unselect_nodes(elements) {
+    let input_el;
+
+    elements.forEach(element => {
+      input_el = element.querySelector('input');
+      if (input_el && input_el.checked) {
+        input_el.click();
+      }
+    });
+  }
+
+
+  deselect_all_nodes() {
+    let input_el;
+
+    this.nodes_arr.forEach(element => {
+      input_el = element.querySelector('input');
+      if (input_el && input_el.checked) {
+        input_el.click();
+      }
+    });
   }
 
   set width(value) {
