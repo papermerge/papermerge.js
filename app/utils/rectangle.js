08280e97c88ae4d847bfe9d2a19c750aa31f046a
Eugen Ciur <eugen@papermerge.com>
2022-02-03 08:23:22 +0200

    refactor rectangle class to use point class

--- app/utils/rectangle.js
+++ app/utils/rectangle.js
@@ -1,3 +1,4 @@
+import Point from 'papermerge/utils/point';
 
 
 export default class Rectangle {
@@ -7,6 +8,11 @@ export default class Rectangle {
     this.y = y;
     this.width = parseInt(width);
     this.height = parseInt(height);
+
+    this.p1 = new Point(this.x, this.y);
+    this.p2 = new Point(this.x + this.width, this.y);
+    this.p3 = new Point(this.x + this.width, this.y + this.height);
+    this.p4 = new Point(this.x, this.y + this.height);
   }
 
   intersect(rect) {
@@ -14,37 +20,37 @@ export default class Rectangle {
     Returns true if this rectangle intersects with rect.
     Two rectangle intersect if one of them has a point inside other.
     */
-    if (this.contains_point(rect.x, rect.y)) {
+    if (this.contains_point(rect.p1)) {
       return true;
     }
 
-    if (this.contains_point(rect.x + rect.width, rect.y)) {
+    if (this.contains_point(rect.p2)) {
       return true;
     }
 
-    if (this.contains_point(rect.x + rect.width, rect.y + rect.height)) {
+    if (this.contains_point(rect.p3)) {
       return true;
     }
 
-    if (this.contains_point(rect.x, rect.y + rect.height)) {
+    if (this.contains_point(rect.p4)) {
       return true;
     }
 
     return false;
   }
 
-  contains_point(x, y) {
+  contains_point(point) {
     /*
       Is point (x, y) inside this rectangle ?
     */
     let x_is_within = false, y_is_within = false;
 
 
-    if (this.x <= x && x <= this.x + this.width) {
+    if (this.x <= point.x && point.x <= this.x + this.width) {
       x_is_within = true;
     }
 
-    if (this.y <= y && y <= this.y + this.height) {
+    if (this.y <= point.y && point.y <= this.y + this.height) {
       y_is_within = true;
     }
 
--- tests/unit/rectangle-test.js
+++ tests/unit/rectangle-test.js
@@ -1,12 +1,57 @@
 import { module, test } from 'qunit';
 import Rectangle from 'papermerge/utils/rectangle';
+import Point from 'papermerge/utils/point';
 
 
 module('Unit | Rectangle', function () {
-  test('basic', function (assert) {
+  test('basic rectangle creation', function (assert) {
     let rect = new Rectangle(1, 1, 200, 200);
 
     assert.ok(rect);
+  });
+
+  test('contains_point positive', function(assert) {
+    let rect, point;
+
+    rect = new Rectangle(100, 100, 10, 10);
+    point  = new Point(105, 105);
+
+    assert.true(
+      rect.contains_point(point)
+    )
+  });
+
+  test('contains_point negative', function(assert) {
+    let rect, point;
+
+    rect = new Rectangle(100, 100, 10, 10);
+    point  = new Point(50, 50);
+
+    assert.false(
+      rect.contains_point(point)
+    )
+  });
+
+  test('p1, p2, p3, p4', function(assert) {
+    let rect;
+
+    rect = new Rectangle(100, 200, 10, 5);
+
+    assert.true(
+      rect.p1.isEqual(new Point(100, 200))
+    );
+
+    assert.true(
+      rect.p2.isEqual(new Point(110, 200))
+    );
+
+    assert.true(
+      rect.p3.isEqual(new Point(110, 205))
+    );
+
+    assert.true(
+      rect.p4.isEqual(new Point(100, 205))
+    );
   });
 });
 
