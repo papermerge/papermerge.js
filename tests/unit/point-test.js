10e9006564a3f0ca6ceda2a749da2dfbe3a29f54
Eugen Ciur <eugen@papermerge.com>
2022-02-03 08:30:55 +0200

    add negative for point class inaquality

--- tests/unit/point-test.js
+++ tests/unit/point-test.js
@@ -3,10 +3,18 @@ import Point from 'papermerge/utils/point';
 
 
 module('Unit | Point', function () {
-  test('basic', function (assert) {
+  test('basic equality positive', function (assert) {
     let p1 = new Point(1, 1), p2 = new Point(1, 1);
 
     assert.true(
+      p1.isEqual(p2)
+    );
+  });
+
+  test('basic equality negative', function (assert) {
+    let p1 = new Point(1, 1), p2 = new Point(1, 2);
+
+    assert.false(
       p1.isEqual(p2)
     );
   });
