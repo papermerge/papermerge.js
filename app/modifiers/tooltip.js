8feaa958184343c078fbc4e00625cf7bc46668df
Eugen Ciur <eugen@papermerge.com>
2022-02-12 08:27:39 +0100

    minor refactoring in tooltip modifier

--- app/modifiers/tooltip.js
+++ app/modifiers/tooltip.js
@@ -4,20 +4,16 @@ import bootstrap from 'bootstrap';
 
 export default class TooltipModifier extends Modifier {
 
-  tooltip = undefined;
-
   didReceiveArguments() {
     let { title } = this.args.named;
 
-    if (!this.tooltip) {
-      this.tooltip = new bootstrap.Tooltip(
-        this.element,
-        {
-          'title': title,
-          'placement': 'right',
-          'trigger': 'hover'
-        }
-      );
-    }
+    return new bootstrap.Tooltip(
+      this.element,
+      {
+        'title': title,
+        'placement': 'right',
+        'trigger': 'hover'
+      }
+    );
   }
 }
