7d316dcfdc4334d6693bfacfa2b581dae9d14dd8
Eugen Ciur <eugen@papermerge.com>
2022-02-26 07:54:59 +0100

    service to trigger inbox refresh

--- app/controllers/authenticated.js
+++ app/controllers/authenticated.js
@@ -1,12 +1,24 @@
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 
 export default class AuthenticatedController extends Controller {
 
   // sidebar expanded ?
   @tracked expanded = true;
+  @service ws_inbox_refresh;
+  @service router;
+
+  constructor() {
+    super(...arguments);
+    this.ws_inbox_refresh.addHandler(this.wsInboxRefreshHandler, this);
+  }
+
+  wsInboxRefreshHandler() {
+    this.router.refresh();
+  }
 
   @action
   onSidebarToggle() {
