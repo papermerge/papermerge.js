826db1571e46339c55df2a3ace1d8f3bbd44d426
Eugen Ciur <eugen@papermerge.com>
2022-02-27 08:33:38 +0100

    redirect user to home folder if he/she trise to access 'authenticated.index' route

---
+++ app/routes/authenticated/index.js
@@ -0,0 +1,18 @@
+import { service } from '@ember/service';
+import BaseRoute from 'papermerge/routes/base';
+
+
+export default class IndexRoute extends BaseRoute {
+  /*
+  The sole purpose of this route is to redirect user
+  to 'home folder' when user try to access root url
+  */
+  @service currentUser;
+
+  async model() {
+    this.transitionTo(
+      'authenticated.nodes',
+      this.currentUser.user.home_folder.get('id')
+    );
+  }
+}
