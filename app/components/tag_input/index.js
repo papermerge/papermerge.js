d7377034f2863cd8f5c04de6defea065b315de4c
Eugen Ciur <eugen@papermerge.com>
2022-04-23 07:25:24 +0200

    pretty good input tag component

--- app/components/tag_input/index.js
+++ app/components/tag_input/index.js
@@ -12,16 +12,27 @@ export default class TagInputComponent extends Component {
   @service store;
 
   @action
-  onAddTag() {
-    let tags, found_tag, new_value;
+  onAddTag(event) {
+    /*
+    Keydown triggered on space/comma.
 
-    tags = this.store.peekAll('tag');
+    Adds tag specified by name `this.new_tag_value` to
+    the list of tags associated with given node.
+    */
+    let found_tag, new_value;
+
+    // prevents space/comma character itself to be included in <input />
+    event.preventDefault();
+
     new_value = this.new_tag_value.trim();
-    found_tag = tags.find(item => item.name == new_value);
+    found_tag = this.all_local_tags.find(item => item.name == new_value);
 
     if (found_tag) {
+      // user chose to associate node one of existing tags
       this.tags.push(found_tag);
     } else {
+      // user wishes to create a new tag
+      // at this point tag will use default color.
       this.tags.push({
         name: new_value
       });
@@ -33,6 +44,15 @@ export default class TagInputComponent extends Component {
   @action
   onRemoveTagItem(index) {
     this.tags.splice(index, 1);
+  }
+
+  get all_local_tags() {
+    /*
+    Returns all local tags (tags available in local ember data)
+    */
+    return this.store.peekAll('tag').map(
+      item => item
+    );
   }
 
   get placeholder() {
