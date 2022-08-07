a671b67d49557c30d1257ac9ded1be62a86144ea
Eugen Ciur <eugen@papermerge.com>
2022-08-07 08:33:27 +0200

    better rename node implementation

--- app/components/modal/rename_node.js
+++ app/components/modal/rename_node.js
@@ -1,5 +1,6 @@
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 import BaseComponent from "./base";
 
@@ -7,27 +8,34 @@ import BaseComponent from "./base";
 export default class RenameNodeComponent extends BaseComponent {
   @service store;
   @service currentUser;
+  @tracked _title = undefined;
 
-
-  get new_title() {
-
-    if (this.title) {
-      return this.title;
+  get title() {
+    if (this._title === undefined) {
+      return this.default_title;
     }
 
-    if (this.node) {
-      return this.node.title
-    }
-
-    return '';
+    return this._title;
   }
 
-  set new_title(value) {
-    this.title = value;
+  set title(value) {
+    this._title = value;
+  }
+
+  get default_title() {
+    if (this.node) {
+      return this.node.title;
+    }
+
+    return 'noname';
   }
 
   get node() {
     return this.args.node;
+  }
+
+  reset() {
+    this._title = undefined;
   }
 
   @action
@@ -35,13 +43,13 @@ export default class RenameNodeComponent extends BaseComponent {
     this.node.title = this.title;
     this.node.save();
     this.args.onClose();
-    this.title = '';
+    this.reset();
   }
 
   @action
   onCancel() {
     this.args.onClose();
-    this.title = '';
+    this.reset();
   }
 
 }
