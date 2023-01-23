5fffe0b064e2c5d17957a71318d3416e3a70185a
Eugen Ciur <eugen@papermerge.com>
2023-01-23 08:38:45 +0100

    read breadcrumb as an array

--- app/components/breadcrumb/index.js
+++ app/components/breadcrumb/index.js
@@ -4,17 +4,7 @@ import Component from '@glimmer/component';
 export default class BreadcrumbComponent extends Component {
 
   get nodes() {
-    let current,
-      _nodes = [];
-
-    current = this.args.node;
-
-    while (current && current.get('id')) {
-      _nodes.push(current);
-      current = current.get('parent');
-    }
-
-    return _nodes.reverse();
+    return this.args.node.breadcrumb;
   }
 
   get query() {
--- app/models/folder.js
+++ app/models/folder.js
@@ -1,7 +1,11 @@
+import { attr } from '@ember-data/model';
+
 import NodeModel from './node';
 
 
 export default class FolderModel extends NodeModel {
+
+  @attr breadcrumb;
 
   get nodeType() {
     return 'folder';
